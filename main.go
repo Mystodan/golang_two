e7f56ad3da0713b1b088eebfd138b2ee1b9d3f20
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-02-10 18:55:20 +0100

    added perf flag and etc.

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
+	start := time.Now()
 	rand.Seed(time.Now().UTC().UnixNano())
+
+	var hasFlag bool
 
 	var flagGen int
 	flag.IntVar(&flagGen, "gen", 0, "generates a list of n random float32's from 0.01 to 0.99 ")
@@ -27,6 +30,8 @@ func main() {
 	flag.BoolVar(&flagComp, "comp", false, "compares the data from the transaction files, with fees and earnings")
 	var flagMiln bool
 	flag.BoolVar(&flagMiln, "mill", false, "Generates a million transactions")
+	var flagPerf bool
+	flag.BoolVar(&flagPerf, "perf", false, "indicates (print) performance (total time to do the given workflow).")
 
 	flag.Parse()
 
@@ -43,24 +48,40 @@ func main() {
 	}
 
 	if flagMiln {
+		hasFlag = true
 		gla2.GenerateMillionTxs()
 		gla2.GenerateFees()
 		gla2.GenerateEarnings()
 	}
 	if flagGen > 0 && !flagMiln {
+		hasFlag = true
 		gla2.GenerateRandomTxs(flagGen)
 		gla2.GenerateFees()
 		gla2.GenerateEarnings()
 	}
 	if len(flagSum) > 4 {
+		hasFlag = true
 		gla2.Sum(gla2.OpenFile(flagSum))
 		fmt.Println("sum of", flagSum, ":", gla2.R2Dec(gla2.Sum(gla2.OpenFile(flagSum))))
-	} else {
+	} else if len(flagSum) != 0 {
 		gla2.Sum()
 	}
 	if flagComp {
+		hasFlag = true
 		Number1, Number2 := gla2.Compare()
 		fmt.Println("comparing Number1 to Number2: ", Number1, ": ", Number2)
+	}
+	if !hasFlag {
+		gla2.GenerateMillionTxs()
+		gla2.GenerateFees()
+		gla2.GenerateEarnings()
+		num1, num2 := gla2.Compare()
+		fmt.Println("Result:", num1, ", ", num2)
+	}
+	if flagPerf {
+		t := time.Now()
+		elapsed := t.Sub(start)
+		fmt.Println("Elapsed time after given workflow: ", gla2.R2Dec(elapsed.Seconds()))
 	}
 
 	if *memprofile != "" {
